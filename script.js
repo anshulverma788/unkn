f14900a800b2decb732327f59fce01e339e041f0
Anshul Verma <168357508+anshulverma788@users.noreply.github.com>
2025-06-13 02:44:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const menuBare = document.querySelector(".menu-bare");
+const icom = document.querySelector("#icon");
+
+icon.onclick = function() {
+    menuBare.classList.toggle("open");
+    if(menuBare.classList.contains("open")){
+        menuBare.style.display = "block";
+        icom.classList.add("fa-xmark")
+        icom.classList.remove("fa-bars")
+    }else{
+         menuBare.style.display = "none";
+        icom.classList.add("fa-bars")
+        icom.classList.remove("fa-xmark")
+    }
+}
